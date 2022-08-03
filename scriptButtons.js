d0a6854e473383c9f8334616aaa32342760b35bf
Felipe Seabra <feliperoce@gmail.com>
2022-08-03 10:48:39 -0300

    Atualizando scriptButtons.js

---
+++ scriptButtons.js
@@ -0,0 +1,11 @@
+
+const addLinkButton = (classe, link) => {
+  const buttons = document.getElementsByClassName(classe);
+  for (let button of buttons) {
+    button.href = link;
+  }
+}
+
+addLinkButton('link-instagram', 'https://www.instagram.com/felipeseabradev/');
+addLinkButton('link-linkedin', 'https://www.linkedin.com/in/felipe-seabra/');
+addLinkButton('link-github', 'https://github.com/felipe-seabra');
